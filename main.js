dee8317333a277e318c7fd494ad9d99148373fd1
alelax <alessandro.lausdei@gmail.com>
2018-04-29 13:25:14 +0200

    Milestone 4 - ok

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
+// 4283f11a-0c36-490a-a135-7df8f7c954d4
 
 /* Film requested HTML structure */
-// var filmDivOpen = "<div class='film'>",
-//       fTitleOpen = "<h1>",
-//       fTitleClose = "</h1>",
-//       fOriginalTitleOpen = "<h2>",
-//       fOriginalTitleClose = "</h2>",
-//       fOriginalLanguageOpen = "<h3>",
-//       fOriginalLanguageClose = "</h3>",
-//       fVoteOpen = "<h4>",
-//       fVoteClose = "</h4>",
-//       fStarOpen = "<div class='stars'>",
-//          starsEmpty = "<i class='fa fa-star-o fa-lg' aria-hidden='true'></i>",
-//          starsFill = "<i class='fa fa-star fa-lg' aria-hidden='true'></i>",
-//       fStarClose = "</div>",
-//     filmDivClose = "</div>";
+var   posterBoxO = "<div class='poster-container'>",
+         infoBoxO = "<div class='info hidden'>",
+            titleO = "<h1 class='title'>",
+            titleC = "</h1>",
+            origTitleO = "<h2 class='original-title'><span class='structure'>Original title: </span>",
+            origTitleC = "</h2>",
+            langOpen = "<h3>",
+            langClose = "</h3>",
+            langLiteralO = "<span class='structure'>Language: ",
+            langLiteralC = " - </span>",
+            langFlagO = "<span class='flag'>",
+            langFlagC = "</span>",
+            voteO = "<div> <span class='structure'>Vote: </span>",
+            voteNumberO = "<span class='vote-number'>  ",
+            voteNumberC = "</span>"
+            voteC = "</div>",
+            posterImageO = "<img src='https://image.tmdb.org/t/p/w342",
+            posterImageC = "'>",
+         infoBoxC = "</div>",
+      posterBoxC = "</div>"
+       ;
 
-// 4283f11a-0c36-490a-a135-7df8f7c954d4
 $(document).ready(function(){
 
    //Salvo in una variabile l'oggetto input
@@ -29,24 +36,68 @@ $(document).ready(function(){
       if (e.which == 13) {
          thisInputVal = thisInput.val();
          resetInput(thisInput);
-         $('.film-list').html('');
+
+         //Cancello i risultati di una eventuale richiesta precedente
+         resetList('.film-list', "Film");
+         resetList('.series-list', "Serie TV");
+
          ajaxMoviesCall(thisInputVal);
          ajaxSeriesCall(thisInputVal);
-
       }
    });
+
+
+
+
 
    //Al click del button search recupero il contenuto dell'input
    //e invio una richiesta AJAX alla API TMDb.
    $('#search-btn').click(function(){
       thisInputVal = thisInput.val();
       resetInput(thisInput);
+
       //Cancello i risultati di una eventuale richiesta precedente
-      $('.film-list').html('');
+      resetList('.film-list', "Film");
+      resetList('.series-list', "Serie TV");
+
       ajaxMoviesCall(thisInputVal);
       ajaxSeriesCall(thisInputVal);
    });
 
+   //Le info del film vengo visualizzate passando sulla copertina del film
+   //e  nascoste uscendo da
+   // $(document).on('mouseenter mouseleave', '.film', function(){
+   //    $(this).find('.overlay').fadeToggle(500);
+   //    $(this).find('.info').fadeToggle(500);
+   //    $(document).on('mouseenter mouseleave', '.info', function(){
+   //       $(this).children('.overview').fadeToggle(500);
+   //    });
+   // });
+   $(document).on('mouseenter mouseleave', '.film', function(){
+      $(this).find('.overlay').fadeToggle(500);
+      $(this).find('.info').fadeToggle(500);
+      $(this).find('.info').on({
+         mouseenter : function(){
+            $(this).find('.overview').fadeIn(500);
+         },
+         mouseleave : function(){
+            $(this).find('.overview').fadeOut(500);
+         }
+      });
+   });
+
+   $(document).on('mouseenter mouseleave', '.serie', function(){
+      $(this).find('.overlay').fadeToggle(500);
+      $(this).find('.info').fadeToggle(500);
+      $(this).find('.info').on({
+         mouseenter : function(){
+            $(this).find('.overview').fadeIn(500);
+         },
+         mouseleave : function(){
+            $(this).find('.overview').fadeOut(500);
+         }
+      });
+   });
 
    /* ***** FUNZIONI ***** */
 
@@ -118,7 +169,7 @@ $(document).ready(function(){
 
 
    function showResults(object) {
-      for (var i = 0; i < object.length; i++) {
+      for (var i = 0; i < object.length ; i++) {
 
          var currentResult = object[i];
          var containerClass = "";
@@ -126,7 +177,20 @@ $(document).ready(function(){
 
          //Se il titolo del film coincide con quello originale, il titolo verrà mostrato solo una volta
          if (currentResult.title == currentResult.original_title) {
-            currentResult.original_title = "";
+            currentResult.original_title = "---";
+         }
+
+         //Se l'overview non è presente stampo un messaggio di avviso
+         if (currentResult.overview == "") {
+            currentResult.overview = "Overview is not available";
+         }
+
+         //Se il poster non è presente inserisco un immagine di default
+         var posterImageTemp = "";
+         if (currentResult.poster == null) {
+            posterImageTemp = "<img src='png/movie-poster-coming-soon.png";
+         } else {
+            posterImageTemp = posterImageO + currentResult.poster;
          }
 
          //Assgno la classe container nella quale verranno inseriti i film oppure quella in cui verrano
@@ -142,19 +206,24 @@ $(document).ready(function(){
          //Inserisco nell'html tutti i dati di ogni singolo film trovato
          $(containerClass).append(
             "<div class='" + typeResultClass + "'>" +
-               "<div class='poster-container'>" +
-                  "<img src='https://image.tmdb.org/t/p/w342" + currentResult.poster + "'>" +
-               "</div>" +
-               "<div class='info'>" +
-                  "<h1>" + currentResult.title + "</h1>" +
-                  "<h2>" + currentResult.original_title + "</h2>" +
-                  // Link a CountryFlagsAPI, restituisce la bandiera in base alla nazione passata
-                  "<h3>" + getFlag(currentResult.original_language) + "</h3>" +
-                  "<h4>" + currentResult.vote + "</h4>" +
-                  "<div class='movie-stars'>" + getRating(currentResult.vote) + "</div>" +
-               "</div>" +
+               posterBoxO +
+                  posterImageTemp + posterImageC +
+
+                  infoBoxO +
+                     "<div class='overview'>" + /* "<span class='structure'>Overview: </span>" + */ currentResult.overview + "</div>" +
+                     // Link a CountryFlagsAPI, restituisce la bandiera in base alla nazione passata
+                     langOpen + langLiteralO + currentResult.original_language + langLiteralC + langFlagO + getFlag(currentResult.original_language) + langFlagC + langClose +
+                     voteO + getRating(currentResult.vote) + voteNumberO + currentResult.vote + voteNumberC + voteC +
+                     origTitleO + currentResult.original_title + origTitleC +
+                     titleO + currentResult.title + titleC +
+                  infoBoxC +
+
+                  "<div class='overlay'></div>" +
+               posterBoxC+
             "</div>"
          );
+
+
       }
    }
 
@@ -190,8 +259,11 @@ $(document).ready(function(){
       if (lang == "en") {
          lang = "gb";
       }
+      if (lang == "ja") {
+         lang = "jp";
+      }
 
-      html += '<img src="http://www.countryflags.io/'+lang+'/flat/32.png" alt="'+lang+'"'+'>';
+      html += '<img src="http://www.countryflags.io/'+lang+'/flat/24.png">';
 
       return html;
    }
@@ -209,6 +281,7 @@ $(document).ready(function(){
             "original_language" : currentMovie.original_language,
             "vote" : currentMovie.vote_average,
             "poster" : currentMovie.poster_path,
+            "overview" : currentMovie.overview,
             "type" : "film"
          };
 
@@ -229,6 +302,7 @@ $(document).ready(function(){
             "original_language" : currentTvshow.original_language,
             "vote" : currentTvshow.vote_average,
             "poster" : currentTvshow.poster_path,
+            "overview" : currentMovie.overview,
             "type" : "tvShow"
          };
 
@@ -242,6 +316,14 @@ $(document).ready(function(){
          "<div class='film'>" +
             "<h2>" + "La ricerca non ha prodotto risultati" + "</h2>" +
          "</div>"
+      );
+   }
+
+   //Funzione che prende in ingresso i la sezione da resettare e il tipo di sezione
+   //Ad ogni nuova chiamata pulisce la pagina dai risultati precedenti.
+   function resetList(classList, section) {
+      $(classList).html('').append(
+         '<div class="type-header">'+section+"</div>"
       );
    }
 });
